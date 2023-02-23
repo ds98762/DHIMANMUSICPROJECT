752b69a91b5aeb077f52cdae361dc5ac17fc74e2
TeamSukun <115199434+TeamSukun@users.noreply.github.com>
2023-02-23 08:53:30 +0530

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -73,4 +73,4 @@ async def init():
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("AnonX").info("Stopping Music Bot...")
+    LOGGER("AnonX").info("Stopping Sukun Music Bot...")
